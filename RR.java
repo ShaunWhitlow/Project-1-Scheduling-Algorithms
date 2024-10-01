90fb12e864a10d5b14ad1fd13781a462be10b1c4
EthanCouchBSU <143107342+EthanCouchBSU@users.noreply.github.com>
2024-10-01 14:28:30 -0400

    round robin

--- RR.java
+++ RR.java
@@ -1,40 +1,72 @@
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
- * Round-Robin
- */
+ 
+Round-Robin*/
 public class RR implements Scheduler {
+    private List<SimProcess> queue;
+    private List<Integer> waitingTimes;
+    private int timeQuantum;
 
-	@Override
-	public void onProcessArrival(SimProcess p, int time) {
-		// TODO Auto-generated method stub
-	}
+    public RR(int timeQuantumArg){
+        queue = new ArrayList<SimProcess>();
+        waitingTimes = new ArrayList<Integer>();
+        timeQuantum = timeQuantumArg;
 
-	@Override
-	public void onProcessExit(SimProcess p, int time) {
-		// TODO Auto-generated method stub
-	}
+        Clock.ENABLE_INTERRUPT = true;
+        Clock.INTERRUPT_INTERVAL = timeQuantum;
 
-	@Override
-	public void onClockInterrupt(int timeElapsed, int time) {
-		// TODO Auto-generated method stub
-	}
+    }
 
-	@Override
-	public String getAlgorithmName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public void onProcessArrival(SimProcess p, int time) {
+        queue.add(p);
+    }
 
-	@Override
-	public SimProcess currentProcess() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public void onProcessExit(SimProcess p, int time) {
+        //remove current process
+        queue.remove(0);
+        //get waiting time of process adn return
+        int waitingTime = time - p.getTimeOfArrival() - p.getBurstTime();
+        System.out.println(p.getId() + " finished at time " + time + ". Its waiting time is " + waitingTime);
+        waitingTimes.add(waitingTime);
+        System.out.println("Current average waiting time: " + calculateAvgWaiting());
+    }
 
-	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    private double calculateAvgWaiting() {
+        double totalWaiting = 0;
+        for (int waitingTime: waitingTimes) {
+            totalWaiting += waitingTime;
+        }
+        return totalWaiting / waitingTimes.size();
+    }
+
+    @Override
+    public void onClockInterrupt(int timeElapsed, int time) {
+        queue.add(currentProcess());
+        queue.remove(0);
+    }
+
+    @Override
+    public String getAlgorithmName() {
+
+        return ("RR with quantum:"+ timeQuantum);
+    }
+
+    @Override
+    public SimProcess currentProcess() {
+        if (queue.size() == 0) {
+            return null;
+        }
+        return queue.get(0);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
 
 }
